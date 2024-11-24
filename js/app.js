92c177508588f1222eb08dafa5056ad9991ad1d3
Cristopher Martinez <125322557+GriionG@users.noreply.github.com>
2024-11-23 19:53:04 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,12 +1,28 @@
 const container = document.querySelector(".container")
 const coffees = [
-  { name: "Perspiciatis", image: "images/coffee1.jpg" },
-  { name: "Voluptatem", image: "images/coffee2.jpg" },
-  { name: "Explicabo", image: "images/coffee3.jpg" },
-  { name: "Rchitecto", image: "images/coffee4.jpg" },
-  { name: " Beatae", image: "images/coffee5.jpg" },
-  { name: " Vitae", image: "images/coffee6.jpg" },
-  { name: "Inventore", image: "images/coffee7.jpg" },
-  { name: "Veritatis", image: "images/coffee8.jpg" },
-  { name: "Accusantium", image: "images/coffee9.jpg" },
+  { name: "logo", image: "img/Logo-goal.png" },
+  { name: "premier", image: "img/Liga-premier-logo.png" },
+  { name: "ligaMX", image: "img/liga-MX-logo.png" },
+  { name: "bayern", image: "img/foto-7.jpg" },
+  { name: " barca", image: "img/foto-20.jpg" },
+  { name: " chivas", image: "img/foto-30.png" },
+  { name: "arsenal", image: "img/foto-21.jpeg" },
+  { name: "tigres", image: "img/foto-27.png" },
+  { name: "trajetas", image: "img/payment-method.png" },
 ]
+const showCoffees = () => {
+  let output = ""
+  coffees.forEach(
+    ({ name, image }) =>
+      (output += `
+              <div class="card">
+                <img class="card--avatar" src=${image} />
+                <h1 class="card--title">${name}</h1>
+                <a class="card--link" href="#">Taste</a>
+              </div>
+              `)
+  )
+  container.innerHTML = output
+}
+
+document.addEventListener("DOMContentLoaded", showCoffees)
