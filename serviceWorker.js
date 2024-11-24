9edaa047dd82d765eddb9ca5bcc37efb6e82cb1e
Cristopher Martinez <125322557+GriionG@users.noreply.github.com>
2024-11-23 20:36:49 -0600

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -7,18 +7,22 @@ const assets = [
   "/img/Logo-goal.png",
   "/img/Liga-premier-logo.png",
   "/img/liga-MX-logo.png",
-  "/img/foto-7.jpg",
-  "/img/foto-20.jpg",
   "/img/foto-30.png",
-  "/img/foto-21.jpeg",
   "/img/foto-27.png",
-  "/img/payment-method.png",
 ]
 
 self.addEventListener("install", installEvent => {
   installEvent.waitUntil(
     caches.open(staticDevCoffee).then(cache => {
       cache.addAll(assets)
+    })
+  )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request)
     })
   )
 })
